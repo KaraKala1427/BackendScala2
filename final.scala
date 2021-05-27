44efc57d5c5dd4faf9e2805fb5a5e49be8684f47
Yernar <47604214+KaraKala1427@users.noreply.github.com>
2021-05-27 14:10:40 +0600

    max depth

--- final.scala
+++ final.scala
@@ -41,3 +41,13 @@ object Solution {
         
     }
 }
+
+
+object Solution {
+    def maxDepth(root: TreeNode): Int = {
+        if (root == null)
+          return 0
+
+        1 + Math.max(maxDepth(root.left), maxDepth(root.right))
+    }
+}
